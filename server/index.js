cf530fc54ea74d6b1d3d1f95899be5fb9f0548c8
mathivan07 <mathivan07@gmail.com>
2025-12-28 15:06:25 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -37,31 +37,10 @@ mongoose.connect('mongodb+srv://mathivan007:RveUGmwEdyKNiHV2@shopcartdb.jjtmf79.
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error('MongoDB connection error:', err));
 
-    require('./models/User');
+require('./models/User');
 require('./models/Log');
 
-const allowedOrigins = [
-  'http://localhost:5173', // Your new local port
-  'http://localhost:5174', // Your old local port
-  'https://location-logs.netlify.app' // Your deployed frontend
-];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    // allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-    
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-  },
-  credentials: true, // Useful if you plan to use cookies/sessions later
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors()); // Allow all origins to fix access issues across devices
 
 
 // Routes
